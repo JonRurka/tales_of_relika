08cba9fd8f82b01e4786865bedfbc91509506b6e
Jonathan Rurka <jon@rurka.net>
2025-08-05 21:41:46 -0700

    debug linux crash

--- src/Engine/src/window.cpp
+++ src/Engine/src/window.cpp
@@ -51,7 +51,9 @@ namespace {
 
         switch (source)
         {
-        case GL_DEBUG_SOURCE_API:             std::cout << "Source: API"; break;
+        case GL_DEBUG_SOURCE_API:             std::cout << "Source: API"; 
+            assert(false);
+            break;
         case GL_DEBUG_SOURCE_WINDOW_SYSTEM:   std::cout << "Source: Window System"; break;
         case GL_DEBUG_SOURCE_SHADER_COMPILER: std::cout << "Source: Shader Compiler"; break;
         case GL_DEBUG_SOURCE_THIRD_PARTY:     std::cout << "Source: Third Party"; break;
